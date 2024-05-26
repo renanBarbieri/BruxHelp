3147ddb3c9889086e70b3ed90cf5aeed316e2c9e
Renan Barbieri <renan.hbarbieri@gmail.com>
2024-05-26 11:39:36 -0300

    changing RegisterDefaults and creating RegisterBruxismDefaults

--- app/src/main/java/br/com/bruxismhelper/feature/register/RegisterDefaults.kt
+++ app/src/main/java/br/com/bruxismhelper/feature/register/RegisterDefaults.kt
@@ -1,11 +1,7 @@
 package br.com.bruxismhelper.feature.register
 
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 object RegisterDefaults {
-    val fieldsSpacerHeight = 16.dp
     val fieldsOutsidePadding = 24.dp
-
-    val formTitleSize = 24.sp
 }
---
+++ app/src/main/java/br/com/bruxismhelper/feature/registerBruxism/RegisterBruxismDefaults.kt
@@ -0,0 +1,7 @@
+package br.com.bruxismhelper.feature.registerBruxism
+
+import androidx.compose.ui.unit.dp
+
+object RegisterBruxismDefaults {
+    val fieldsOutsidePadding = 24.dp
+}
