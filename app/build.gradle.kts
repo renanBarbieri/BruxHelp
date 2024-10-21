19fc58aa884b92016d55b7c685a0cce15729da36
Renan Barbieri <renan.hbarbieri@gmail.com>
2024-10-21 13:21:17 -0300

    upgrading libs versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "br.com.bruxismhelper"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
